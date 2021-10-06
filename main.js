324854e0adc3ce3686e833ac9cb45ca1b53e1890
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 10:35:24 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? [
+    parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16)
+  ] : null;
+}
+
+alert(hexToRgb("#0033ff").g); // "51";
+alert(hexToRgb("#03f").g); // "51";
